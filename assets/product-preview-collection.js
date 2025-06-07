482439758af1ae24734911840aab281c04f500c1
ferlarag <89590031+ferlarag@users.noreply.github.com>
2025-06-07 12:30:50 -0600

    add: initial collection grid

---
+++ assets/product-preview-collection.js
@@ -0,0 +1,39 @@
+class ProductCollectionPreview extends HTMLElement {
+  constructor() {
+    super();
+    this.selectedVariant = null;
+    this.previousNode = null;
+  }
+
+  connectedCallback() {
+    const linkButton = this.querySelector('.link-to-product');
+    const skuNode = this.querySelector('.sku');
+    const variantNode = this.querySelector('.product-variant');
+
+    this.querySelectorAll('.product-preview-picker-button').forEach((button, index) => {
+      if (index === 0) {
+        this.selectedVariant = button.getAttribute('data-variant-id');
+        this.previousNode = button;
+      }
+
+      button.addEventListener('click', () => {
+        const productUrl = button.getAttribute('data-product-url');
+        const variant = button.getAttribute('data-variant-id');
+        const variantName = button.getAttribute('data-variant-name');
+        const sku = button.getAttribute('data-variant-sku');
+
+        if (this.selectedVariant !== variant) {
+          this.selectedVariant = variant;
+          this.previousNode.classList.remove('swatch-active');
+          button.classList.add('swatch-active');
+          this.previousNode = button;
+          skuNode.textContent = sku;
+          variantNode.textContent = variantName;
+          linkButton.setAttribute('href', `${productUrl}?variant=${variant}`);
+        }
+      });
+    });
+  }
+}
+
+customElements.define('product-collection-preview', ProductCollectionPreview);
